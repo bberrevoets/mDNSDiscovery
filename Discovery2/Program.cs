0c717121d526ac8e813cd90ee579673ddf4e1a9f
B.N.Berrevoets <bert@berrevoets.net>
2022-11-30 08:51:55 +0100

    Add project files.

---
+++ Discovery2/Program.cs
@@ -0,0 +1,27 @@
+﻿using Zeroconf;
+
+namespace Discovery2
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ProbeForNetworkPrinters();
+        }
+        
+        public static async Task ProbeForNetworkPrinters()
+        {
+            IReadOnlyList<IZeroconfHost> results = await
+                ZeroconfResolver.ResolveAsync("_printer._tcp.local.");
+            Console.WriteLine("");
+        }
+
+        public static async Task EnumerateAllServicesFromAllHosts()
+        {
+            ILookup<string, string> domains = await ZeroconfResolver.BrowseDomainsAsync();            
+            var responses = await ZeroconfResolver.ResolveAsync(domains.Select(g => g.Key));            
+            foreach (var resp in responses)
+                Console.WriteLine(resp);
+        }
+    }
+}
---
+++ mDNSDiscovery/Program.cs
@@ -0,0 +1,20 @@
+﻿using Makaretu.Dns;
+
+namespace mDNSDiscovery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var mdns = new MulticastService();
+            mdns.NetworkInterfaceDiscovered += (s, e) => mdns.SendQuery("_hwenergy._tcp");
+            mdns.AnswerReceived += (s, e) => { 
+                // do something with e.Message
+                e.RemoteEndPoint.Address.ToString();
+            };
+                mdns.Start();
+
+                Console.ReadLine();
+        }
+    }
+}
